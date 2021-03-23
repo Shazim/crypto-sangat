2d038131e3dc122142d61f3b5515419348cc65cb
Shazim Khan <iamshazimkhan@gmail.com>
2021-03-23 12:04:41 +0500

    Telegram Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
           </div>
           <div className="buy-button">
             <a
-              href="http://t.me/chillpk"
+              href="https://t.me/joinchat/SgnrxGbuXjqFnjFG"
               target="_blank"
               className="btn btn-primary"
             >
@@ -54,7 +54,7 @@ class Header extends Component {
             </ul>
             <div className="buy-menu-btn d-none">
               <a
-                href="http://t.me/chillpk"
+                href="https://t.me/joinchat/SgnrxGbuXjqFnjFG"
                 target="_blank"
                 className="btn btn-primary"
               >
